3364a29c24d5c5f566a863b7a2877440fe0fe48c
ly <ly@jianzhimao.com>
2016-06-23 17:09:14 +0800

    应用更新

--- app/src/main/java/com/yoyo/yoappmanage/module/about/AboutActivity.java
+++ app/src/main/java/com/yoyo/yoappmanage/module/about/AboutActivity.java
@@ -1,20 +1,24 @@
 package com.yoyo.yoappmanage.module.about;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.widget.Toolbar;
 import android.text.TextUtils;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
 
+import com.yoyo.common.view.YoSnackbar;
 import com.yoyo.yoappmanage.R;
 import com.yoyo.yoappmanage.base.BaseActivity;
 import com.yoyo.yoappmanage.common.util.AppInfoUtil;
 
 public class AboutActivity extends BaseActivity {
-    Button bt_update;
+    //  Button bt_update;
+    TextView app_version;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,9 +30,9 @@ public class AboutActivity extends BaseActivity {
     }
 
     private void init() {
-        TextView app_version = (TextView) findViewById(R.id.app_version);
-        bt_update = (Button) findViewById(R.id.bt_update);
-        String versionName = "版本 "+AppInfoUtil.getVersionName(AboutActivity.this);
+        app_version = (TextView) findViewById(R.id.app_version);
+        // bt_update = (Button) findViewById(R.id.bt_update);
+        String versionName = "版本 " + AppInfoUtil.getVersionName(AboutActivity.this);
         if (TextUtils.isEmpty(versionName)) {
             app_version.setVisibility(View.GONE);
         } else {
@@ -44,15 +48,56 @@ public class AboutActivity extends BaseActivity {
     }
 
     @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_about, menu);
+        return true;
+    }
+
+    @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
         switch (id) {
             case android.R.id.home:
                 finish();
                 return true;
+            case R.id.action_update:
+                openApplicationMarket();
+                return true;
 
         }
         return super.onOptionsItemSelected(item);
     }
 
+    /**
+     * 通过包名 在应用商店打开应用
+     *
+     */
+    private void openApplicationMarket() {
+        String packageName = this.getPackageName();
+        try {
+            String str = "market://details?id=" + packageName;
+            Intent localIntent = new Intent(Intent.ACTION_VIEW);
+            localIntent.setData(Uri.parse(str));
+            startActivity(localIntent);
+        } catch (Exception e) {
+            // 打开应用商店失败 可能是没有手机没有安装应用市场
+            e.printStackTrace();
+            YoSnackbar.showSnackbar(app_version,R.string.open_market_error);
+
+            // 调用系统浏览器进入商城
+            String url = "https://www.baidu.com/s?wd="+getResources().getString(R.string.app_name);
+            openLinkBySystem(url);
+        }
+    }
+
+    /**
+     * 调用系统浏览器打开网页
+     *
+     * @param url 地址
+     */
+    private void openLinkBySystem(String url) {
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(Uri.parse(url));
+        startActivity(intent);
+    }
 }
