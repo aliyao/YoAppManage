e02b22aa0f00955dafc68e89b5e41543dab1918a
yoyo <905078312@qq.com>
2016-07-25 19:31:49 +0800

    修改信息

--- app/src/main/java/com/yoyo/yoappmanage/config/AppConfig.java
+++ app/src/main/java/com/yoyo/yoappmanage/config/AppConfig.java
@@ -16,16 +16,16 @@ public class AppConfig {
     //上下拉刷新延迟时间
     public final static long RefreshViewTime = 500;//毫秒
     public final static int DBVersion = 1;//数据库版本
-    public final static String DBName = "YoAppManageDB";
+    public final static String DBName = "";
     public final static int minPageSize=10;
 
     public final static boolean Is_SSOSetting=false;
 
-    public final static String APP_AES_KEY="M5LN2zu8p1WEadVn";
-    public final static String APP_MD5_USER_INFO_KEY="DnXZgyZo2QKeZMoQ";
-    public final static String APP_MD5_USER_ID_KEY="jdxOuv211KjVXwcz";
+    public final static String APP_AES_KEY="";
+    public final static String APP_MD5_USER_INFO_KEY="";
+    public final static String APP_MD5_USER_ID_KEY="";
 
-    public final static String MY_BMOB_APPLICATION_ID="0c085439eb211b3a95691c8d4e015297";
+    public final static String MY_BMOB_APPLICATION_ID="";
 
-    public final static String MY_BMOB_UPDATE_APP_CLOUDCODENAME="AppUpdateCheck";
+    public final static String MY_BMOB_UPDATE_APP_CLOUDCODENAME="";
 }
